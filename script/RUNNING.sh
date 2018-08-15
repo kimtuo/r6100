09812ffa6b0ca57cc4710a86089a4545e9990c38
kim <kim.tuo@yuantong.cn>
2018-08-15 11:04:47 +0800

    fix 1

--- script/RUNNING.sh
+++ script/RUNNING.sh
@@ -20,4 +20,31 @@ function reset
 
 }
 
+function check_iprule
+{
+    echo "-----check_iprule-----"
+    rule_count=$(ip rule | wc -l )
+    echo "rule count = $rule_count "
+    if [ $rule_count -lt 2000 ]
+    then
+        echo "add ip rule "
+        for cn_ip in $(cat /script/CN.ip)
+        do
+        #	ip rule del to $cn_ip table cn pref 888
+            ip rule add to $cn_ip table cn pref 888
+        done
+        for yt_ip in $(cat /script/SERVER.ip)
+        do
+        #        ip rule del to $yt_ip table wan pref 889
+                ip rule add to $yt_ip table wan pref 89
+        done
+        ip route flush cache
+    elif [ $rule_count -gt 10000 ]
+    then
+        reboot
+    fi
+
+}
+
 reset
+check_iprule
